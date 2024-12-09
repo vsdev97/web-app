568378edcccc64dbe8e27f7e1b99d2ec32026d80
VenuMadhav Sheshabhattar <vsheshabhattar210@everestdx.com>
2024-12-09 16:23:12 +0530

    Initial commit: Setup React project with esbuild and routing

---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home: React.FC = () => <h1>Welcome to Charity Home</h1>;
+
+export default Home;
---
+++ src/app/router.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const AppRouter: React.FC = () => (
+  <Router basename="/web-app/">
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRouter;
